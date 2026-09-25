d9bfdcb0e8e8d11455bc1afd171dbb5974c0c463
Shubham Nagaria <shubhamnagaria@Shubhams-MacBook-Air.local>
2026-09-25 16:33:09 +0530

    added

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ mcp = FastMCP("My Simple Server")
 def greet(name: str) -> str:
     """A tool that greets a user by name."""
     return f"Hello, {name}! Welcome to FastMCP."
-
+def curse(name:str)->str:
+    "A tool to curse a user by name"
+    return f"Saale ! Shubham ki {name}"
 if __name__=="main":
     mcp.run(transport="http",host="0.0.0.0",port=8000)
