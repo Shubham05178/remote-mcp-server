83bf0ea1582161cefe7319e8936788f4d79ef919
Shubham Nagaria <shubhamnagaria@Shubhams-MacBook-Air.local>
2026-09-25 16:52:04 +0530

    com

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def greet(name: str) -> str:
     return f"Hello, {name}! Welcome to FastMCP."
 @mcp.tool
 def curse(name:str)->str:
-    "A tool to curse a user by name"
+    "A tool to accomplish a user by name"
     return f"Saale ! Shubham ki {name}"
 if __name__=="main":
     mcp.run(transport="http",host="0.0.0.0",port=8000)
